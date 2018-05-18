f7b08ab07d099bc2702d4d8b138f3cb9b1ba0846
DavidFivem <39220253+DavidFivem@users.noreply.github.com>
2018-05-18 16:23:17 +0200

    Add files via upload

---
+++ esx_drugs/config.lua
@@ -0,0 +1,25 @@
+Config              = {}
+Config.MarkerType   = 1
+Config.DrawDistance = 100.0
+Config.ZoneSize     = {x = 5.0, y = 5.0, z = 3.0}
+--Config.MarkerColor  = {r = 100, g = 204, b = 100}
+Config.RequiredCopsCoke = 2
+Config.RequiredCopsMeth = 2
+Config.RequiredCopsWeed = 0
+Config.RequiredCopsOpium = 2
+Config.Locale = 'en'
+
+Config.Zones = {
+	CokeFarm = 		 {x=53.487,           y=3761.814,         z=38.600},
+	CokeTreatment =  {x=-175.141,         y=6170.679,         z=30.200},
+	CokeResell = 	 {x=-11.219,          y=512.038,          z=169.600},
+	MethFarm = 		 {x=498.881, 	      y=2606.475,         z=42.700},
+	MethTreatment =  {x=2432.435,         y=4971.045,         z=41.300},
+	MethResell = 	 {x=1369.192,         y=-1745.180,        z=64.200},
+	WeedFarm = 		 {x=2219.200, 		  y=5577.257,         z=52.800},
+	WeedTreatment =  {x=-1112.687,        y=4904.293,         z=217.500},
+	WeedResell = 	 {x=46.495,           y=-1933.273,        z=20.500},
+	OpiumFarm = 	 {x=2329.410,	      y=2570.104,         z=45.700},
+	OpiumTreatment = {x=950.499,          y=-2123.488,        z=30.400},
+	OpiumResell = 	 {x=-3203.670,        y=1198.959,         z=8.500}
+}
